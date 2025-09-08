0db11c754d226fae5cca49094206f43343f70d27
RomainVoynier <romain.voynier@ynov.com>
2025-09-08 11:58:47 +0200

    EX4

--- EX4/ex4.go
+++ EX4/ex4.go
@@ -1,18 +1,9 @@
 package main
 
 func PGCD(a,b int) int {
-	a = 20
-	b = 10
-	if a < 0 {
-		return 0
-	}
-	if b < 0 { 
-		return 0
-	}
-	if a == b {
-		return 1
-	}
-	else {
-		return a%b
+	a := 20
+	b := 10
+	for !b = 0 {
+		a,b = a%b
 	}
 }
